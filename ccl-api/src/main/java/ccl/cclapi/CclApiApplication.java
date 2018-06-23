239e7d9671a7a44e1e8d448d8babca5854f5d62e
陈亚辉 <chenyahui@dookbook.com>
2018-06-23 14:21:27 +0800

    1

---
+++ ccl-api/src/main/java/ccl/cclapi/CclApiApplication.java
@@ -0,0 +1,12 @@
+package ccl.cclapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CclApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CclApiApplication.class, args);
+    }
+}
---
+++ ccl-api/src/test/java/ccl/cclapi/CclApiApplicationTests.java
@@ -0,0 +1,16 @@
+package ccl.cclapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CclApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
